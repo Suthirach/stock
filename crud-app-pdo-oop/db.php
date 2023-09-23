638a837f1efc8c9b2ea96bd5ffcd750634d78e76
Suthirach <suthirachtanjanthuek@gmail.com>
2023-09-23 17:39:21 +0700

    Create db.php

---
+++ crud-app-pdo-oop/db.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'config.php';
+
+class Database extends Config {
+    public function insert($fname, $lname, $email, $phone) {
+        $sql = "INSERT INTO users (first_name, last_name, email, phone) 
+                VALUES (:fname, :lname, :email, :phone)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([
+            ':fname' => $fname,
+            ':lname' => $lname,
+            ':email' => $email,
+            ':phone' => $phone,
+        ]);
+        return true;
+    }
+
+    public function read() {
+        $sql = "SELECT * FROM users ORDER BY id DESC";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        return $result;
+    }
+}
+?>
