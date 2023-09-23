8423708d0831ae651814962a5262ec4450c7d283
Suthirach <suthirachtanjanthuek@gmail.com>
2023-09-23 17:39:35 +0700

    Create signin_db.php

---
+++ signin_db.php
@@ -0,0 +1,73 @@
+<?php
+
+    session_start();
+    require_once 'config/db.php';
+    // print_r($_POST);
+    if(isset($_POST['signin'])) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+       
+        // } else if (!empty($firstname)) { 
+        //     if(preg_match('/^[a-z]+$/i',$firstname)){ 
+                
+        //     } else {
+        //         $_SESSION['error'] = 'กรุณากรอกชื่อเป็นภาษาอังกฤษ';
+        //         header("location: signin.php"); 
+        //     }
+
+        if (empty($email)) {
+            $_SESSION['error'] = 'กรุณากรอกอีเมล';
+            header("location: signin.php"); 
+        } else if (!filter_var($email,FILTER_VALIDATE_EMAIL)) {
+            $_SESSION['error'] = 'รูปแบบอีเมลไม่ถูกต้อง';
+            header("location: signin.php"); 
+        } else if (empty($password)) {
+            $_SESSION['error'] = 'กรุณากรอกรหัสผ่าน';
+            header("location: signin.php"); 
+        } else if (strlen($_POST['password']) > 20 || strlen($_POST['password']) <  5) {
+            $_SESSION['error'] = 'รหัสผ่านต้องมีความยาวระหว่าง 5 ถึง 20 ตัวอักษร';
+            header("location: signin.php"); 
+
+        } else {
+            try {
+                $check_data  = $conn->prepare("SELECT * FROM users WHERE email = :email");
+                $check_data ->bindParam(":email", $email);
+                $check_data ->execute();
+                $row = $check_data ->fetch(PDO::FETCH_ASSOC);
+                
+                if ($check_data->rowCount() > 0) {
+                   
+                    if ($email == $row['email']){
+                        if (password_verify($password, $row['password'])){
+                            if ($row['urole'] == 'admin') {
+                                $_SESSION['admin_login'] = $row['id'];
+                                header("location: admin.php");
+                            } else {
+                                $_SESSION ['user_login'] = $row['id'];
+                                header("location: user.php");
+                            }
+                        } else {
+                            $_SESSION ['error'] = "รหัสผ่านไม่ถูกต้อง";
+                            header("location: signin.php"); 
+                        }
+                    } else { 
+                        $_SESSION ['error'] = "อีเมลของท่านเหมือนจะผิด";
+                            header("location: signin.php"); 
+                    }
+                } else {
+                    $_SESSION ['error'] = "ไม่มีข้อมูลของท่านในระบบ";
+                    header("location: signin.php"); 
+                }
+ 
+            } catch (PDOException $e){
+                echo $e->getMessage();
+            }
+        }
+    }
+
+
+?>
+
+
+
+
