95050b2b7a73631487fc08762d19ed23c165f961
Uzrax <145991668+Uzrax@users.noreply.github.com>
2023-09-27 01:41:27 +0700

    Update main.js

--- crud-app-pdo-oop/main.js
+++ crud-app-pdo-oop/main.js
@@ -2,6 +2,8 @@ const addFrom = document.getElementById('add-user-form');
 const showAlert = document.getElementById('showAlert');
 const addModal = new bootstrap.Modal(document.getElementById("addNewUserModal"));
 const tbody = document.querySelector("tbody");
+const updateForm =  document.getElementById("edit-user-form");
+const editModal = new bootstrap.Modal(document.getElementById("editUserModal")); 
 
 addFrom.addEventListener("submit", async (e) => {
     e.preventDefault();
@@ -41,6 +43,69 @@ const fetchAllUsers = async () => {
 }
 fetchAllUsers();
 
+tbody.addEventListener("click", (e)=> {
+    if (e.target && e.target.matches("a.editlink")) {
+        e.preventDefault();
+        let id = e.target.getAttribute("id");
+        editUser(id);
+    }
+})
 
 
+const editUser = async (id) => {
+    const data = await fetch(`action.php?edit=1&id=${id}`, {
+        method: "GET"
+    })
+    const response = await data.json();
+    document.getElementById("id").value = response.id;
+    document.getElementById("fname").value = response.firstname;
+    document.getElementById("lname").value = response.lastname;
+    document.getElementById("email").value = response.email;
+    document.getElementById("phone").value = response.phone;
+}
 
+updateForm.addEventListener("submit", async(e) => {
+    e.preventDefault();
+
+    const formData = new FormData(updateForm);
+    formData.append("update", 1);
+
+    if (updateForm.checkValidity() === false) {
+        e.preventDefault();
+        e.stopPropagation();
+        addForm.classList.add("was-validated");
+        return false;
+    } else {
+        document.getElementById("edit-user-btn").value = "Plase Wait...";
+
+        const data = await fetch("action.php", {
+            method: "POST",
+            body: formData 
+        })
+        const response = await data.text();
+        showAlert.innerHTML = response;
+        document.getElementById("edit-user-btn").value = "Edit User";
+        updateForm.reset();
+        updateForm.classList.remove("was-validated");
+        editModal.hide();
+        fetchAllUsers();
+
+    }
+})
+
+tbody.addEventListener("click", (e)=> {
+    if (e.target && e.target.matches("a.deletelink")) {
+        e.preventDefault();
+        let id = e.target.getAttribute("id");
+        deleteUser(id); 
+    }
+})
+
+const deleteUser = async (id) => {
+    const data = await fetch(`action.php?delete=1&id=${id}` , {
+        method: "GET"
+    })
+    const response = await data.text();
+    showAlert.innerHTML = response;
+    fetchAllUsers(); 
+}
